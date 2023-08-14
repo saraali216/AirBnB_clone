12b31b278b7198daf33f1a2ada3869aa97900c39
Ichrafsassi <ichraf.sassi.456@gmail.com>
2023-08-13 19:31:19 -0700

    updated

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+""" test Amenity"""
 from models.amenity import Amenity
+from tests.test_models.test_base_model import test_basemodel
 
 
 class test_Amenity(test_basemodel):
-    """ """
+    """ test amenity """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ init """
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
-    def test_name2(self):
-        """ """
+    def test_scndname(self):
+        """ testing second name """
         new = self.value()
         self.assertEqual(type(new.name), str)
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,99 +1,99 @@
 #!/usr/bin/python3
-""" """
-from models.base_model import BaseModel
+""" test base_model """
 import unittest
-import datetime
-from uuid import UUID
 import json
 import os
+from models.base_model import BaseModel
+import datetime
+from uuid import UUID
 
 
 class test_basemodel(unittest.TestCase):
-    """ """
+    """ testing base module """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """init """
         super().__init__(*args, **kwargs)
         self.name = 'BaseModel'
         self.value = BaseModel
 
-    def setUp(self):
-        """ """
-        pass
-
-    def tearDown(self):
+    def delete_storage_file(self):
         try:
             os.remove('file.json')
         except FileNotFoundError:
             pass
 
+    def test_id(self):
+        """test id """
+        new_id = self.value()
+        self.assertEqual(type(new_id.id), str)
+
+    def setUp (self)
+        """setup"""
+        pass
+
     def test_default(self):
-        """ """
-        i = self.value()
-        self.assertEqual(type(i), self.value)
+        """default testing """
+        tdf = self.value()
+        self.assertEqual(type(tdf), self.value)
 
     def test_kwargs(self):
-        """ """
-        i = self.value()
-        copy = i.to_dict()
+        """test kwargs """
+        tkwa = self.value()
+        copy = tkwa.to_dict()
         new = BaseModel(**copy)
-        self.assertFalse(new is i)
+        self.assertFalse(new is tkwa)
 
     def test_kwargs_int(self):
-        """ """
-        i = self.value()
-        copy = i.to_dict()
+        """ testing kwargs int"""
+        tkwai = self.value()
+        copy = tkwai.to_dict()
         copy.update({1: 2})
         with self.assertRaises(TypeError):
             new = BaseModel(**copy)
 
     def test_save(self):
-        """ Testing save """
-        i = self.value()
-        i.save()
-        key = self.name + "." + i.id
+        """ testing save """
+        ts = self.value()
+        ts.save()
+        key = self.name + "." + ts.id
         with open('file.json', 'r') as f:
-            j = json.load(f)
-            self.assertEqual(j[key], i.to_dict())
+            i = json.load(f)
+            self.assertEqual(i[key], ts.to_dict())
 
     def test_str(self):
-        """ """
-        i = self.value()
-        self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
-                         i.__dict__))
+        """ testing the str """
+        tstr = self.value()
+        self.assertEqual(str(tstr), '[{}] ({}) {}'.format(self.name, tstr.id,
+                         tstr.__dict__))
 
     def test_todict(self):
-        """ """
-        i = self.value()
-        n = i.to_dict()
-        self.assertEqual(i.to_dict(), n)
+        """ test to distionary """
+        ttdic = self.value()
+        n = ttdic.to_dict()
+        self.assertEqual(ttdic.to_dict(), n)
 
     def test_kwargs_none(self):
-        """ """
-        n = {None: None}
+        """test no kwargs ?"""
+        x = {None: None}
         with self.assertRaises(TypeError):
-            new = self.value(**n)
+            new = self.value(**x)
 
     def test_kwargs_one(self):
-        """ """
-        n = {'Name': 'test'}
+        """test kwargs one """
+        x = {'Name': 'test'}
         with self.assertRaises(KeyError):
-            new = self.value(**n)
+            new = self.value(**x)
 
-    def test_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.id), str)
+    def test_created_attribute(self):
+        """test created attribute"""
+        new_att = self.value()
+        self.assertEqual(type(new_att.created_attribute), datetime.datetime)
 
-    def test_created_at(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.created_at), datetime.datetime)
-
-    def test_updated_at(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.updated_at), datetime.datetime)
-        n = new.to_dict()
-        new = BaseModel(**n)
-        self.assertFalse(new.created_at == new.updated_at)
+    def test_updated_attribute(self):
+        """test updated attribute """
+        new_att = self.value()
+        self.assertEqual(type(new_att.updated_at), datetime.datetime)
+        n = new_att.to_dict()
+        new_att = BaseModel(**n)
+        self.assertFalse(new_att.created_attribute == new_att.updated_attribute)
