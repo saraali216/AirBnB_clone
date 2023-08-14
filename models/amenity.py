7d34a21137f98655dd7be38e6fdf47dee631bf91
Ichrafsassi <ichraf.sassi.456@gmail.com>
2023-08-13 17:54:58 -0700

    updated

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+""" State Module for HBNB project 
+    The `amenity` module"""
 from models.base_model import BaseModel
 
-
 class Amenity(BaseModel):
+    """Amenity() is a sub-class of "BaseModul()
+        Provided by place or house
+    """
     name = ""
